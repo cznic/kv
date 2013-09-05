cf1879a0893eabb22ebc258484900bac3586be70
Jan Mercl <jnml@fsc-r550.(none)>
2013-09-05 14:14:07 +0200

    kvaudit: Fix godocs

--- kvaudit/main.go
+++ kvaudit/main.go
@@ -16,15 +16,15 @@ Usage:
 
 Options:
 
-    -max	maximum number of errors to report. Default 10.
-    		The actual number of reporetd errors, if any, may be less
-		because many errors do not allow to reliably continue the audit.
-
-    -v		List every error in addition to the overall one.
+	-max	maximum number of errors to report. Default 10.	The actual
+		number of reporetd errors, if any, may be less 	because many
+		errors do not allow to reliably continue the audit.
+	
+	-v	List every error in addition to the overall one.
 
 Arguments:
 
-    file	Example: ~/foo/bar.db
+	file	For example: ~/foo/bar.db
 
 Implementation Notes
 
